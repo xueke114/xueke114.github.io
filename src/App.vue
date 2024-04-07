9bbf3dd5dcdff0a8352e23b844b5a977c8adf169
Zheng Xueke <xueke0114@foxmail.com>
2024-04-07 16:53:33 +0800

    change to vue& blog to oldBlog

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <MainWindow />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MainWindow from './components/MainWindow.vue';
+
+export default defineComponent({ name: "App", components: { MainWindow } })
+</script>
+<style>
+</style>
